98a5f38feec8bc32f4562321a595152bcace4310
GouthamReddy7 <gowthamr733@gmail.com>
2023-09-23 23:08:12 +0530

    working

---
+++ src/main/java/com/goutham/kaibur/repository/ServerRepository.java
@@ -0,0 +1,12 @@
+package com.goutham.kaibur.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.goutham.kaibur.model.Server;
+
+
+
+public interface ServerRepository extends MongoRepository<Server, Integer> {
+
+
+}
